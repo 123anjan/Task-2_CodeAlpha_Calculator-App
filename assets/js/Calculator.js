d05e13e6b56678ba0ed5fbefa6b25b095a90d1ef
Anjan <anjan0306basak@gmail.com>
2026-09-15 20:27:57 +0530

    Updated Calculator.js

--- assets/js/Calculator.js
+++ assets/js/Calculator.js
@@ -19,6 +19,32 @@ export class Calculator {
     clearEntry() {
         this.currentOperand = '0';
     }
+
+    deleteDigit() {
+        if (this.isEvaluated) {
+            this.clearAll();
+            return;
+        }
+        if (this.currentOperand.length === 1 || this.currentOperand === '0') {
+            this.currentOperand = '0';
+        } else {
+            this.currentOperand = this.currentOperand.slice(0, -1);
+        }
+    }
+
+    appendNumber(number) {
+        if (this.isEvaluated) {
+            this.currentOperand = number === '.' ? '0.' : number;
+            this.isEvaluated = false;
+            return;
+        }
+        if (number === '.' && this.currentOperand.includes('.')) return;
+        if (this.currentOperand === '0' && number !== '.') {
+            this.currentOperand = number;
+        } else {
+            this.currentOperand += number;
+        }
+    }
 }
 
 
